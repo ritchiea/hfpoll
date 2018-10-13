49272a8317564235297b83cb5473a980e548ce83
Andrew Ritchie <hello@andrewritchie.info>
2018-10-13 13:49:21 +0200

    Add socket.io

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const server = express()
+const http = require('http').Server(express)
+const io = require('socket.io')(http);
+
+server.use(express.static(path.join(__dirname, 'public')))
+server.set('views', path.join(__dirname, 'views'))
+server.set('view engine', 'ejs')
+server.get('/', (req, res) => res.render('pages/index'))
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
