f669db17ef2a41f646213d5e784215aa6b962580
Andrew Ritchie <hello@andrewritchie.info>
2018-10-13 17:01:15 +0200

    Working client admin

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000
 const server = express()
 const http = require('http').Server(server)
 const io = require('socket.io')(http)
+const data = {};
 
 server.use(express.static(path.join(__dirname, 'public')))
 server.set('views', path.join(__dirname, 'views'))
@@ -14,9 +15,10 @@ server.get('/secretadminaaa', (req, res) => res.render('pages/admin'))
 
 io.on('connection', (socket) => {
   console.log('Client connected');
-  socket.on("name", function(msg) {
-    console.log(`NAME: ${msg}`);
-    socket.emit("update", msg);
+  socket.on("answer", function(msg) {
+    data[msg["id"]] = msg
+    console.log(data)
+    socket.broadcast.emit("update", data);
   });
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
